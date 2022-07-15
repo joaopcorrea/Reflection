a9299b476ed438b78d8ef58fb586dcdf792eee3b
João Pedro Corrêa <joaopedrocorrea21@gmail.com>
2022-07-15 14:57:37 -0300

    feat: implementing the exercise

--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+namespace Reflection;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Display Public Properties:");
+        DisplayPublicProperties();
+
+        Console.WriteLine("\nCreate Instance:");
+        CreateInstance();
+    }
+
+    static void DisplayPublicProperties()
+    {
+        var type = GetTypeOf<Student>();
+
+        var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        foreach (var property in properties)
+        {
+            Console.WriteLine("{0}", property.Name);
+        }
+    }
+
+    static void CreateInstance()
+    {
+        var type = GetTypeOf<Student>();
+
+        var instance = (Student)Activator.CreateInstance(type);
+
+        var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        foreach (var property in properties)
+        {
+            property.SetValue(instance, GetValueOfType(property.PropertyType));
+        }
+
+        var methodInfo = type.GetMethod("DisplayInfo");
+        methodInfo!.Invoke(instance, null);
+    }
+
+    private static object? GetValueOfType(Type type)
+    {
+        if (type.Equals(typeof(string)))
+        {
+            return "Valor";
+        }
+        else if (type.Equals(typeof(int)))
+        {
+            return 10;
+        }
+
+        return null;
+    }
+
+    static Type? GetTypeOf<T>()
+    {
+        var types = Assembly.GetExecutingAssembly().GetTypes();
+
+        foreach (var type in types)
+        {
+            if (type.Equals(typeof(T)))
+            {
+                return type;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ Reflection/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string University { get; set; }
+        public int RollNumber { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"{Name} - {University} - {RollNumber}");
+        }
+    }
+}
